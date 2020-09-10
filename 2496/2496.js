d21b1d8c52355ea5ff1a212648b670fb0b62e876
微信公众号少年歌行PRO <69911266+sngxpro@users.noreply.github.com>
2020-09-10 16:38:38 +0800

    Update 2496.js

--- 2496/2496.js
+++ 2496/2496.js
@@ -1,7 +1,16 @@
 /*
-[rewrite_local]
 
+微信公众号：少年歌行PRO 制作
+电报资源发布群（禁言纯净）@shao66 
+电报水友交流群（资源同步）@sngxpro
+电报私聊 @shaogebot
+QQ群：654371081
+
+圈X配置：
+
+[rewrite_local]
 ^http?:\/\/api\_\d{4}\.kuwo\.cn\/front\/user\/vipstatus$ url script-response-body 2046.js
+
 [mitm]
 hostname = api_2496.kuwo.cn
 
