a7a2590d367eb582765172cb0f5092079a1a7ba6
微信公众号少年歌行PRO <69911266+sngxpro@users.noreply.github.com>
2021-02-08 11:13:17 +0800

    Update pythonai.js

--- pythonai/pythonai.js
+++ pythonai/pythonai.js
@@ -3,7 +3,7 @@
 QX:
 [rewrite_local]
 #解锁会员
-^https:\/\/ws\.60he\.com\/book*|user* url script-response-body https://raw.githubusercontent.com/sngxpro/QuantumultX/master/pythonai/pythonai.js
+^https:\/\/ws\.60he\.com\/book|user* url script-response-body https://raw.githubusercontent.com/sngxpro/QuantumultX/master/pythonai/pythonai.js
 
 [mitm]
 hostname = ws.60he.com,
@@ -11,8 +11,17 @@ hostname = ws.60he.com,
 
 let obj = JSON.parse($response.body);
 obj = {
-  "isFree": 1,
-  "vip": 365,
+    "amount": 0,
+    "alert": false,
+    "sysId": 0,
+    "nickName": "少年歌行pro",
+    "chargeTotal": 0,
+    "stars": 0,
+    "userName": "",
+    "vip": 365,
+    "userId": 11518,
+    "login_sum": 36
+    "isFree":1,
   };
 
 $done({body: JSON.stringify(obj)});
