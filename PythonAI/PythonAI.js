167ce03cd2546c91ea9e3abe1e9c98adb2364837
微信公众号少年歌行PRO <69911266+sngxpro@users.noreply.github.com>
2021-02-15 21:14:16 +0800

    Update PythonAI.js

--- PythonAI/PythonAI.js
+++ PythonAI/PythonAI.js
@@ -12,17 +12,36 @@ QQ群：654371081
 hostname = ws.60he.com
 */
 
+/*
+微信公众号：少年歌行PRO 制作
+电报资源发布群（禁言纯净）@shao66 
+电报水友交流群（资源同步）@sngxpro
+电报私聊 @shaogebot
+QQ群：654371081
+圈X配置：
+[rewrite_local]
+^https:\/\/ws\.60he\.com\/(book.*|user.*) url script-response-body PythonAI.js
+[mitm]
+hostname = ws.60he.com
+
+*/
+
 var body = $response.body;
 var url = $request.url;
+var obj = JSON.parse(body);
 
-const path1 = '\/user*';
-const path2 = '\/book*'
+const a = '/user*';
+const b = '/book*';
 
-let obj = JSON.parse(body);
+if (url.indexOf(a) != -1) {
+    obj.data.item.nickname = "公众号：少年歌行pro",
+    obj.data.item.vip = 365,
+    body = JSON.stringify(obj);
+}
+if (url.indexOf(b) != -1) {
+    obj.data.isFree = 1,
+    body = JSON.stringify(obj);
+}
 
-if (url.indexOf(path1|path2) != -1) {
-	obj.data["isFree"] = 1;
-	obj.data["vip"] = 365;
-	body = JSON.stringify(obj);  
- }
+
 $done({body});
