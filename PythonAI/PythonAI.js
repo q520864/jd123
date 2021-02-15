db7457082afbf23cec10e98c32c3ca49a350b35e
微信公众号少年歌行PRO <69911266+sngxpro@users.noreply.github.com>
2021-02-15 21:14:45 +0800

    Update PythonAI.js

--- PythonAI/PythonAI.js
+++ PythonAI/PythonAI.js
@@ -12,19 +12,6 @@ QQ群：654371081
 hostname = ws.60he.com
 */
 
-/*
-微信公众号：少年歌行PRO 制作
-电报资源发布群（禁言纯净）@shao66 
-电报水友交流群（资源同步）@sngxpro
-电报私聊 @shaogebot
-QQ群：654371081
-圈X配置：
-[rewrite_local]
-^https:\/\/ws\.60he\.com\/(book.*|user.*) url script-response-body PythonAI.js
-[mitm]
-hostname = ws.60he.com
-
-*/
 
 var body = $response.body;
 var url = $request.url;
