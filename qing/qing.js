9c8635e31d37d97fb54ca5dd193770a0fe1be683
微信公众号少年歌行PRO <69911266+sngxpro@users.noreply.github.com>
2021-02-15 22:12:32 +0800

    Update qing.js

--- qing/qing.js
+++ qing/qing.js
@@ -9,6 +9,11 @@ QQ群：654371081
 ^http?:\/\/cn1\.invit\.vip\/users\/info url script-response-body https://raw.githubusercontent.com/sngxpro/QuantumultX/master/qing/qing.js
 [mitm]
 hostname = cn1.invit.vip
+
+[app下载地址]
+电子请柬制作 - 婚礼邀请函制作必备APP
+appstore下载，https:\/\/apps.apple.com\/cn\/app\/%E7%94%B5%E5%AD%90%E8%AF%B7%E6%9F%AC%E5%88%B6%E4%BD%9C-%E5%A9%9A%E7%A4%BC%E9%82%80%E8%AF%B7%E5%87%BD%E5%88%B6%E4%BD%9C%E5%BF%85%E5%A4%87app\/id1354806829
+
 */
 
 var body = $response.body;
