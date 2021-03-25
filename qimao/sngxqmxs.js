890eedd111a8de3d4e8eee763e2c65ca9cb21249
微信公众号少年歌行PRO <69911266+sngxpro@users.noreply.github.com>
2021-03-25 11:18:26 +0800

    Update sngxqmxs.js

--- qimao/sngxqmxs.js
+++ qimao/sngxqmxs.js
@@ -7,7 +7,7 @@
 [rewrite_local]
 #七猫小说解锁vip
 
-https://(.+)\.wtzw\.com/.+ url script-response-body qm.js
+https://(.+)\.wtzw\.com/.+ url script-response-body https://github.com/sngxpro/QuantumultX/raw/master/qimao/sngxqmxs.js
 
 [mitm]
 hostname= *.wtzw.com
