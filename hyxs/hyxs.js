f6a6be3487f099209bc7637c1699f6354ee6a7e8
微信公众号少年歌行PRO <69911266+sngxpro@users.noreply.github.com>
2021-04-19 11:37:22 +0800

    Update hyxs.js

--- hyxs/hyxs.js
+++ hyxs/hyxs.js
@@ -1,8 +1,7 @@
 /*
  biz.lemonyd.com
  
-https://biz.lemonyd.com(\/user\/account\/info|\/activity\/activity\/account) url script-response-body https://raw.githubusercontent.com/sngxpro/QuantumultX/master/hyxs/hyxs.js
-
+https://biz.lemonyd.com(\/user\/account\/info|\/activity\/activity\/account) url script-response-body https://raw.githubusercontent.com/sngxpro/QuantumultX/master/hyxs/hyxs.js
 
 */
 
